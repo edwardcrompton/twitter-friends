c05a1d80be7677e3b72587fb7bd4e52570ad4741
Edward Crompton <edward.crompton@cameronandwilding.com>
2016-12-23 10:15:02 +0000

    Adding the Guzzle library and the FollowersController

--- app/Http/Controllers/Twitter/FollowersController.php
+++ app/Http/Controllers/Twitter/FollowersController.php
@@ -7,13 +7,16 @@
 namespace App\Http\Controllers\Twitter;
 
 use App\Http\Controllers\Controller;
-
+use GuzzleHttp\Client;
 
 class FollowersController extends Controller {
+
+
   /**
    * Show the followers of the handle.
    */
   public function showFollowers($handle) {
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
     return view('reports.followers', ['handle' => $handle]);
   }
 }
