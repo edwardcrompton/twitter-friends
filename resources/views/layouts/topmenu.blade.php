14fd952bd2f8775e3cbaf37f3e5315aef131c79a
Ed Crompton <edward.crompton@tuttlemail.co.uk>
2017-01-23 15:08:00 +0000

    Adding a partial for the top menu and adding a configurable main twitter handle

---
+++ resources/views/layouts/topmenu.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default">
+    <div class="navbar-header"><a class="navbar-brand" href="#">Twitter Friends</a></div><!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/{{ $mainhandle }}/friends/celebs">Friends: Celebs</a></li>
+                        <li><a href="/{{ $mainhandle }}/friends/lastupdated">Friends: Old profiles</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="/{{ $mainhandle }}/followers/celebs">Followers: Celebs</a></li>
+                    </ul>
+          </li>
+      </ul>
+</nav>
