5a6aa279c0aa6281b02676dc6e072302616ca659
Edward Crompton <edward.crompton@cameronandwilding.com>
2017-04-12 21:54:06 +0100

    Adding a @todo

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/{screenName}/followers/{sorting}', 'Twitter\FollowersController@sho
 //
 Route::get('/{screenName}/unfollowers', 'Twitter\FollowersController@showUnfollowers');
 
-// I think this should be moved somewhere special.        
+// @todo: I think this should be moved somewhere special.
 App::bind('LengthAwarePaginator', function ($app, $params) {
   return new LengthAwarePaginator($params[0], $params[1], $params[2]);
 });
