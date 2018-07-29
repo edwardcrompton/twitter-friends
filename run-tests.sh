85e0d22b328e5ed9a3c51d0fd30df3891ff955d3
Edward Crompton <edward.crompton@gmail.com>
2018-07-29 16:52:32 +0100

    Trying to get logs to show in Travis

--- run-tests.sh
+++ run-tests.sh
@@ -10,17 +10,14 @@ until [ "$COMPOSER_INSTALLING" == "false" ]; do
   sleep 1
 done
 
-echo "*** Showing permissions ***"
-docker-compose exec php ls -lha /app/storage/
-docker-compose exec php ls -lha /app/bootstrap/
-
 echo "*** Testing artisan ***"
 docker-compose exec php php artisan env -vvv
 
 echo "*** Running tests ***"
 docker-compose exec php php artisan dusk -vvv
 
-cat storage/logs/laravel.log
+# This needs to be changed to use a dynamic date.
+cat storage/logs/laravel-2018-07-29.log
 
 echo "*** Shutting down ***"
 exec docker-compose down
