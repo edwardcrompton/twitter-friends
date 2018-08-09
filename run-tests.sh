468614a248f6538d1844fed3e8d1dbff540ff665
Edward Crompton <edward.crompton@gmail.com>
2018-08-09 22:44:45 +0100

    Setting permissions in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,8 @@ docker-compose up -d --build
 echo "Waiting for composer to install..."
 docker wait twitterfriends_composer_1 > /dev/null
 
+# Setting file permissions.
+docker-compose exec php chmod -R 777 storage
+docker-compose exec php chmod -R 777 bootstrap/cache
+
 docker-compose exec php php artisan dusk
