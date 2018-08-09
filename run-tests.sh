49416aeb5ab287842228770cabe36489de44ff06
Edward Crompton <edward.crompton@gmail.com>
2018-08-09 20:10:07 +0100

    Correcting docker permissions config

--- run-tests.sh
+++ run-tests.sh
@@ -5,13 +5,16 @@ docker-compose up -d --build
 COMPOSER_INSTALLING=1
 
 until [ "$COMPOSER_INSTALLING" == "false" ]; do
-  COMPOSER_INSTALLING=$(docker-compose ps -q composer | xargs docker inspect composer -f '{{ .State.Running }}')
+  COMPOSER_INSTALLING=$(docker-compose ps -q composer | xargs docker inspect  -f '{{ .State.Running }}')
   >&2 echo "Composer is installing"
   sleep 1
 done
 
 echo "*** Testing artisan ***"
 docker-compose exec php php artisan env -vvv
+
+docker-compose exec php chmod -R 777 storage
+docker-compose exec php chmod -R 777 bootstrap/cache
 
 docker-compose exec php ls -lah storage
 docker-compose exec php ls -lah bootstrap
