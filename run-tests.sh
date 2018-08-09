ec886f943230e9a611440d5e6d6ba0fa641c7ac6
Edward Crompton <edward.crompton@gmail.com>
2018-08-09 19:31:25 +0100

    Correcting a call to docker inspect

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ docker-compose up -d --build
 COMPOSER_INSTALLING=1
 
 until [ "$COMPOSER_INSTALLING" == "false" ]; do
-  COMPOSER_INSTALLING=$(docker-compose ps -q composer | xargs docker inspect -f '{{ .State.Running }}')
+  COMPOSER_INSTALLING=$(docker-compose ps -q composer | xargs docker inspect composer -f '{{ .State.Running }}')
   >&2 echo "Composer is installing"
   sleep 1
 done
